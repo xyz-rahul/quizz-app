dee68893629c0aeec9270f024df889312d361f2f
Rahul Kumar <me.rahulkumar@icloud.com>
2024-03-11 01:47:04 +0530

    added cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import QuizRoutes from './route/quiz'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const express = require('express')
 const app = express()
 
 app.use(bodyParser.json())
+app.use(cors())
 app.use('/quiz', QuizRoutes)
 
 const PORT = process.env.PORT
